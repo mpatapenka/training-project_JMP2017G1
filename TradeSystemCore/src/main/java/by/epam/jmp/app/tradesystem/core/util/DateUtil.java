7440a4ad33df39d973d323beb35729114ddb80d0
Maksim Patapenka <maksim.patapenka@gmail.com>
2017-03-06 01:56:55 +0300

    Main: Updated build configuration.

--- TradeSystemCore/src/main/java/by/epam/jmp/app/tradesystem/core/util/DateUtil.java
+++ TradeSystemCore/src/main/java/by/epam/jmp/app/tradesystem/core/util/DateUtil.java
@@ -3,10 +3,21 @@ package by.epam.jmp.app.tradesystem.core.util;
 import java.sql.Date;
 import java.util.Calendar;
 
+/**
+ * Util class for working with dates
+ */
 public final class DateUtil {
 
+    /**
+     * Util class cannot be instantiated
+     */
     private DateUtil() { }
 
+    /**
+     * Get today date
+     *
+     * @return today java.sql.Date
+     */
     public static Date getTodaySqlDate() {
         Calendar calendar = Calendar.getInstance();
         long timeInMillis = calendar.getTimeInMillis();
